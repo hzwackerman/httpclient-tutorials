7ef9c0458c628825d5e90a60c4eff2e923fb4bec
hzwackerman <168236704@qq.com>
2017-06-03 11:57:05 +0800

    remove useless import

--- src/main/java/org/example/Example_01.java
+++ src/main/java/org/example/Example_01.java
@@ -1,11 +1,8 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
